07516fcab57381db90201d95fa51a54b9090d573
Sammy <o.samy39@gmail.com>
2021-10-13 23:41:32 +0530

    mqtt-client setup and publishing locations data to location_updates topic

---
+++ mqtt/publishLocations.js
@@ -0,0 +1,30 @@
+const mqtt = require('mqtt')
+
+const locations = require('./locations.json')
+
+const publishLocations = () =>{
+
+    const client = mqtt.connect('mqtt://broker.hivemq.com/mqtt')
+
+    client.on('connect', ()=>{
+        client.publish('test','Hello MQTT')
+    })
+
+    const publish = (topic,message) =>{
+        if(client.connected){
+            client.publish(topic,message)
+        }
+    }
+
+    let index = 0
+
+    const timer_ID = setInterval(()=>{
+        publish('location_updates', JSON.stringify(locations.features[index++]))
+        if(index===locations.features.length){
+            client.end()
+        }
+    },3000)
+
+}
+
+module.exports = publishLocations
